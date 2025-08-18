f1a2c4299cf8d6d8ec5cc15fc28227caaee87f57
Mikens404 <ron0563@gmail.com>
2025-08-19 00:49:06 +0900

    edit TrainDomain

--- internal/domain/train.go
+++ internal/domain/train.go
@@ -1,13 +1,30 @@
 package domain
 
+type TrainType int
+
+const (
+	Local          TrainType = iota // 普通
+	Rapid                           // 快速
+	Express                         // 急行
+	LimitedExpress                  // 特急
+)
+
 type Train struct {
-	trainNo   int
-	timeTable TimeTable
-	trainType int // string 絶対めんどくさい
-	seatMap   SeatMap
+	trainNo           int
+	timeTable         TimeTable
+	trainType         TrainType
+	reservationStatus []ReservationStatus
 }
 
-type SeatMap struct { // 予約状況も一緒に格納?
+type ReservationStatus struct {
+	carNumber    int             // 何両目
+	reservations map[string]bool // [席番]
 }
 
-type TimeTable struct{}
+type TimeTable []StationStopList
+
+type StationStopList struct {
+	StationName   string
+	ArrivalTime   int // ex.1000 {秒単位で管理しない(したくない)ので}
+	DepartureTime int
+}
