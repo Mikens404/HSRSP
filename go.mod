bb5f3e8e1d2ed915d31afa23368c299429bafbb3
Mikens404 <ron0563@gmail.com>
2025-08-19 03:07:10 +0900

    add yamal

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Mikens404/HSRSP
+
+go 1.24.6
--- main.go
+++ main.go
@@ -5,3 +5,5 @@ import "fmt"
 func main() {
 	fmt.Print("Hello world!")
 }
+
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen@latest --target internal/presentation --clean api/openapi.yaml
