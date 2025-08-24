3379ca9e4ec4b9f27ed3ef910b47483aecc9e929
Mikens404 <ron0563@gmail.com>
2025-08-25 05:02:30 +0900

    インターフェースの使用

--- internal/application/train_service.go
+++ internal/application/train_service.go
@@ -6,24 +6,24 @@ import (
 	"github.com/Mikens404/HSRSP/internal/domain"
 )
 
-type TrainService struct {
+type TrainService interface {
+	GetTrainInfo(ctx context.Context, trainNumber int) (domain.Train, error)
+}
+
+type trainService struct {
 	trainRepository domain.TrainRepository
 }
 
 func NewTrainService(
 	trainRepository domain.TrainRepository,
 ) TrainService {
-	return TrainService{
+	return &trainService{
 		trainRepository: trainRepository,
 	}
 }
 
-/* type TrainService interface {
-	GetTrainInfo(ctx context.Context, trainNumber int) (domain.Train, error)
-} */
-
-func (t TrainService) GetTrainInfo(ctx context.Context, trainNumber int) (domain.Train, error) {
-	trainInfo, err := t.trainRepository.GetTrainInfo(ctx, trainNumber)
+func (t *trainService) GetTrainInfo(ctx context.Context, trainNumber int) (domain.Train, error) {
+	trainInfo, err := t.trainRepository.FindTrainInfo(ctx, trainNumber)
 	if err != nil {
 		return domain.Train{}, err
 	}
