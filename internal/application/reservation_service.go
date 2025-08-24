a42e8aa85fd818d61ca49f448f925366db77c590
Mikens404 <ron0563@gmail.com>
2025-08-24 16:44:49 +0900

    handler 周りの実装

--- internal/application/reservation_service.go
+++ internal/application/reservation_service.go
@@ -6,6 +6,15 @@ import (
 	"github.com/Mikens404/HSRSP/internal/domain"
 )
 
+type ReservationService interface {
+	GetReservationSeat(ctx context.Context, trainNumber int, carNumber int) (domain.SeatReservationStatus, error)
+}
+
+/*
+	 type reservationServiceImpl struct {
+		reservationRepository domain.ReservationRepository
+	}
+*/
 func GetReservationSeat(ctx context.Context, trainNumber int, carNumber int) (domain.SeatReservationStatus, error) {
 	return domain.SeatReservationStatus{}, nil
 }
--- internal/presentation/handler.go
+++ internal/presentation/handler.go
@@ -6,8 +6,20 @@ import (
 	"github.com/Mikens404/HSRSP/internal/application"
 )
 
-func GetReservationSeat(ctx context.Context, params GetReservationSeatParams) (GetReservationSeatOK, error) {
-	// application.GetReservationSeat が (domain.SeatReservationStatus, error) を返すと仮定
+type handler struct {
+	reservationService application.ReservationService
+}
+
+func NewHandler(
+	reservationService application.ReservationService,
+) handler {
+	return handler{
+		reservationService: reservationService,
+	}
+}
+
+// 号車ごとの予約状況
+func (h handler) GetReservationSeat(ctx context.Context, params GetReservationSeatParams) (GetReservationSeatOK, error) {
 	rs, err := application.GetReservationSeat(ctx, params.TrainNumber, params.CarNumber)
 	if err != nil {
 		return nil, err
