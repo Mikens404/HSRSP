6203f4e4b0d3ae7ac7cd2d448a5bc0c392fa4d15
Mikens404 <ron0563@gmail.com>
2025-08-25 19:07:01 +0900

    refactor

--- internal/domain/calculate.go
+++ internal/domain/calculate.go
@@ -1,22 +1,19 @@
 package domain
 
 func CalculateDistance(boardingStation string, getOffStation string) int {
+	// todo　実装
 	return 1
 }
 
 func CalculateFare(distance int) int {
-	var responseAmount int
 	switch {
 	case distance <= 0:
-		responseAmount = 0
+		return 0
 	case distance <= 10:
-		responseAmount = 500
+		return 500
 	case distance <= 20:
-		responseAmount = 600
-	case distance <= 30:
-		responseAmount = 700
+		return 600
 	default:
-		responseAmount = 700
+		return 700
 	}
-	return responseAmount
 }
